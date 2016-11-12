68d6426582f561cff4561ad01481da437c35bf49
AnabolKiev <anabol.ua@gmail.com>
2016-11-12 11:31:07 +0200

    MonafacturerDownload class

---
+++ src/main/java/com/anabol/motosale/dao/repository/ManufacturerDownloadRepository.java
@@ -0,0 +1,7 @@
+package com.anabol.motosale.dao.repository;
+
+import com.anabol.motosale.model.ManufacturerDownload;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ManufacturerDownloadRepository extends CrudRepository<ManufacturerDownload, Long>{
+}
---
+++ src/main/java/com/anabol/motosale/model/ManufacturerDownload.java
@@ -0,0 +1,10 @@
+package com.anabol.motosale.model;
+
+public class ManufacturerDownload {
+    public String url;
+    public String manufacturer;
+    public boolean isChecked;
+    public int pagesCount;
+    public int modelsCount;
+}
+
